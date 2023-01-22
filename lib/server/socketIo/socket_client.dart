4132c044e943d31653e6e4179a7e3b236df94e0e
Devendra K. Bharti <54721915+kumardev0614@users.noreply.github.com>
2023-01-23 02:18:05 +0530

    Everything is working perfectly

--- lib/server/socketIo/socket_client.dart
+++ lib/server/socketIo/socket_client.dart
@@ -1,14 +1,13 @@
 import 'dart:developer';
 import 'package:livekit_client/livekit_client.dart';
 import 'package:socket_io_client/socket_io_client.dart';
-import 'package:flutter_webrtc/flutter_webrtc.dart' as rtc;
 
 late Socket socket;
-connectToServer() {  
+connectToServer() {
   try {
     socket = io(
         // herokusocketurl,
-        'http://192.168.181.51:3000',
+        'http://192.168.200.51:3000',
         OptionBuilder()
             .setTransports(['websocket']) // for Flutter or Dart VM
             .disableAutoConnect() // disable auto-connection
@@ -20,9 +19,12 @@ connectToServer() {
     socket.on('connect', (_) {
       log(socket.id!); //outputs the socket id
     });
+    socket.on("receiveToken", (token) async {
+      await livekitCall(token);
+    });
 
     socket.emit('chat message', 'Hello from flutter app');
-    socket.on("chat message", (data) => log("recived: $data"));
+    socket.on("chat message", (data) => log("Chat msg: $data"));
   } catch (e) {
     log("============= ERROR in connection: $e ====================");
   }
@@ -30,33 +32,32 @@ connectToServer() {
 
 randomCall(int deviceId) {
   try {
+    // livekitCall();
     socket.emit('call', deviceId);
-    socket.on("receiveToken", (token) {
-      log("The Token is: $token");
-      livekitCall(token);
-      // Get.snackbar("Random Call", "Calling...");
-    });
   } catch (e) {
     log("============= ERROR in randomCall: $e ====================");
   }
 }
 
 Room room = Room();
+
 // livekit call
 livekitCall(token) async {
-  log("token is passed:$token");
+  // log("token is passed:$token");
 
-  final roomOptions = RoomOptions(
+  const roomOptions = RoomOptions(
     adaptiveStream: true,
     dynacast: true,
-    // ... your room options
   );
-  await room.connect('wss://ot-dev.livekit.cloud',
-      "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6Im15Um9vbSJ9LCJpYXQiOjE2NzQyODI3MDAsIm5iZiI6MTY3NDI4MjcwMCwiZXhwIjoxNjc0MzA0MzAwLCJpc3MiOiJBUElpdmlzanZuQlNwZWEiLCJzdWIiOiIyMzA0MDAiLCJqdGkiOiIyMzA0MDAifQ.LMKjQ6DBVCTn2X4ymbbLC0MKnuGoW9xdhlbGjC2q89Y",
+  // await room.connect('wss://demo.livekit.cloud',
+  //     "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ2aWRlbyI6eyJyb29tSm9pbiI6dHJ1ZSwicm9vbSI6IndhcnpvbmUiLCJjYW5QdWJsaXNoIjp0cnVlLCJjYW5TdWJzY3JpYmUiOnRydWV9LCJpYXQiOjE2NzQ0MDI5NDEsIm5iZiI6MTY3NDQwMjk0MSwiZXhwIjoxNjc0NDEwMTQxLCJpc3MiOiJBUElreldoYnhCYUdTaXEiLCJzdWIiOiJiaXN3YWppdCIsImp0aSI6ImJpc3dhaml0In0.ryCOz3M8RUO49rG9kDgdxWroufPVcUZpm5aY6WvOH6E",
+  //     roomOptions: roomOptions);
+
+  await room.connect('wss://ot-dev.livekit.cloud', token,
       roomOptions: roomOptions);
 
   await room.localParticipant?.setMicrophoneEnabled(true);
-  await rtc.Helper.setSpeakerphoneOn(true);
+  // await rtc.Helper.setSpeakerphoneOn(true);
 }
 
 sendData() {
@@ -70,7 +71,7 @@ sendData() {
 
 disconnect() async {
   await room.disconnect();
-  await room.dispose();
-  log("room disposed!!!");
+  // await room.dispose();
+  log("room disconnected!!!");
   // socket.disconnect();
 }
