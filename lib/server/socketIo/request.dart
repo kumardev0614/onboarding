7f5adf2364d18da79774030942f186d21c2e161a
Devendra K. Bharti <54721915+kumardev0614@users.noreply.github.com>
2023-01-27 15:56:31 +0530

    changed server

--- lib/server/socketIo/request.dart
+++ lib/server/socketIo/request.dart
@@ -2,15 +2,21 @@ import 'package:onboarding/server/socketIo/socket_client.dart';
 
 import 'package:http/http.dart' as http;
 
+import '../livekit/livekit.dart';
+
 Future makeGetRequest() async {
   final response =
-      await http.get(Uri.parse('http://192.168.187.49:3000?id="jeet"'));
+      // await http.get(Uri.parse('http://192.168.187.49:3000?id="jeet"'));
+      await http
+          .get(Uri.parse('http://142.93.222.141:3000/?id=%22devendra%22'));
 
   if (response.statusCode == 200) {
     print(response.body);
   } else {
     print('Request failed with status: ${response.statusCode}');
   }
+
+  return response.body;
 }
 
 // sendData() {
@@ -25,7 +31,9 @@ Future makeGetRequest() async {
 Future randomCall(int deviceId) async {
   try {
     // livekitCall();
-    socket.emit('call', deviceId);
+    // socket.emit('call', deviceId);
+    var token = await makeGetRequest();
+    await livekitCall(token);
   } catch (e) {
     print("============= ERROR in randomCall: $e ====================");
   }
