dcc2fbbd3770353703092ee26b0d34b4b0781a60
Devendra K. Bharti <54721915+kumardev0614@users.noreply.github.com>
2023-01-29 14:19:36 +0530

    server changed

--- lib/server/socketIo/socket_client.dart
+++ lib/server/socketIo/socket_client.dart
@@ -6,7 +6,7 @@ late Socket socket;
 connectToServer() {
   socket = io(
       // herokusocketurl,
-      'http://192.168.2.51:3000/',
+      'http://142.93.222.141:3000/',
       OptionBuilder()
           .setTransports(['websocket']) // for Flutter or Dart VM
           .disableAutoConnect() // disable auto-connection
